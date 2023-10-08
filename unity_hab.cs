ce7d7c09d8aa6db92f7f1f23e1121463fb62b9a4
AlokPy1484 <alokpandey1483@gmail.com>
2023-10-08 22:13:08 +0530

    comments

--- unity_hab.cs
+++ unity_hab.cs
@@ -1,6 +1,13 @@
+
+//Replace it with value of slider
 int test = 34;
+//Replace with minimun value for habitibility
 int mi = 1;
+//Replace with maximum value for habitibility 
 int max = 100;
+
+
+
 string habit = "NON ";
 
 if (int i = test; test >= mi || test <= max;){
