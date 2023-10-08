f6cfeda867998616c343a3e28e88a7e83dc82e73
AlokPy1484 <alokpandey1483@gmail.com>
2023-10-08 16:57:59 +0530

    new fresh one

--- nas_unity.cs
+++ nas_unity.cs
@@ -13,55 +13,12 @@ public class TextScript : MonoBehaviour
 
         var c = "100";
 
-        int ran = 0;
-        StreamReader reader = new StreamReader("/Users/alokpandey/Desktop/aaa/nasa_data_f.csv");
-        data = reader.ReadLine();
-        // Console.WriteLine(data);
-        List<string> listA = new List<string>();
-
-
-
-        //to select a random planet from our data set-----------------
-        for(int i = 0; i < ran;i++){
-            data = reader.ReadLine();}
-            // Console.WriteLine(data);}
-
-        var values = data.Split(','); 
-
-        //to add each parameter in list one by one-----------------
-        foreach (var item in values)
-        {   listA.Add(item);
-
-        }
-
-
-        // while (data != null){
-        //     foreach (var item in values){
-        //     listA.Add(item);}
-        // }
-
-
-        // Console.WriteLine(listA.Count);
-
-
-        //to make a variable for each parameter of planet-----------------------------
-        string name = listA[0];
-        string sun_n = listA[1];
-        string year = listA[2];
-        string orb_p = listA[3];
-        string radi = listA[4];
-        string mass = listA[5];
-        string temp = listA[6];
-
-
         text.text = name ;
+        //myText.GetComponent<Text›().text = "second one";
 
-        foreach (var dat in listA){
-        Console.WriteLine(dat);}
 
-        reader.Close();
 
-        
+     
     }
 
     public void NextTextUpdate()
